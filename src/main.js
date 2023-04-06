cc924dc46831f5c93f883fe33245f29aab8d45a8
Oybek Salomov <oybeksalomov@gmail.com>
2023-04-06 17:57:36 +0500

    add vuex (user.js, store.js)

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/plugins/router";
+import store from "@/plugins/vuex/store";
 
 createApp(App)
     .use(router)
+    .use(store)
     .mount('#app')
 
---
+++ src/plugins/vuex/user.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+export default {
+    actions: {
+        fetchToken(context, data) {
+            return new Promise((resolve, reject) => {
+                axios
+                    .post('http://localhost:8880/api/users/auth', data)
+                    .then((response) => {
+                        console.log('Token olindi')
+                        console.log(response)
+
+                        context.commit('updateToken', response.data.token)
+                        resolve()
+                    })
+                    .catch(() => {
+                        console.log('Token olishda xatolik bor')
+                        reject()
+                    })
+                    .finally(() => {
+                        console.log('Bu funksiya har doim ishlarkan')
+                    })
+            })
+        }
+    },
+    mutations: {
+        updateToken(state, token) {
+            state.token = token
+        }
+    },
+    state: {
+        token: null
+    },
+    getters: {
+        getToken(state) {
+            return state.token
+        }
+    },
+}
